6bc2909bb75b9076de23fb231ce4af35d14bcaf0
jimin yun <ge72946@gmail.com>
2024-01-08 22:10:46 +0900

    editComment and MutationResponse

--- users/createAccount/createAccount.typeDefs.js
+++ users/createAccount/createAccount.typeDefs.js
@@ -2,11 +2,6 @@ import { gql } from "apollo-server";
 
 export default gql`
 
-type createAccountResult {
-    ok: Boolean!
-    error: String
-  }
-
  type Mutation {
     createAccount(
       firstName: String!
@@ -14,7 +9,7 @@ type createAccountResult {
       username: String!
       email: String!
       password: String!
-    ): createAccountResult!
+    ): MutationResponse!
   }
 
 `;
