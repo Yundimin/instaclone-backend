af85ac5d8fafeabe3c96a18294bc01329e72222b
jimin yun <ge72946@gmail.com>
2024-01-14 21:46:29 +0900

    authenticate subscription

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import http from "http";
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
@@ -10,22 +11,42 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
-  context: async ({ req }) => {
-    return {
-      loggedInUser: await getUser(req.headers.token),
-    };
-    // context: {
-    //   token:
-    //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzAzNzM5MTIyfQ.K4c3BeOJrgil0AgQ534uiDZ0A_K3yqSGlRkFjOPObZg",
-    // },
+  context: async (ctx) => {
+    if (ctx.req) {
+      return {
+        loggedInUser: await getUser(ctx.req.headers.token),
+      };
+    } else {
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedInUser: context.loggedInUser,
+      };
+    }
+  },
+  subscriptions: {
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("You can't listen.");
+      }
+      const loggedInUser = await getUser(token);
+      return {
+        loggedInUser,
+      };
+    },
   },
 });
+
 
 const app = express();
 app.use(logger("tiny"));
 apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
-app.listen({ port: PORT }, () => {
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`🚀Server is running on http://localhost:${PORT}/graphql ✅`);
 });
 // require("dotenv").config();
